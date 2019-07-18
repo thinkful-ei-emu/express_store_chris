6886910661f6c247736faad02d0c23f49a5a0eb6
Chris Martin <chris.bizunit34@gmail.com>
2019-07-18 16:52:38 -0400

    added all of the testing for the various API calls

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
+const uuid = require('uuid/v4');
 
 const app = express();
 
@@ -15,9 +16,131 @@ const morganOption = (NODE_ENV === 'production')
 app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
+app.use(express.json());
+
+const users = [
+  {
+    'id': '3c8da4d5-1597-46e7-baa1-e402aed70d80',
+    'username': 'sallyStudent',
+    'password': 'c00d1ng1sc00l',
+    'favoriteClub': 'Cache Valley Stone Society',
+    'newsLetter': 'true'
+  },
+  {
+    'id': 'ce20079c-2326-4f17-8ac4-f617bfd28b7f',
+    'username': 'johnBlocton',
+    'password': 'veryg00dpassw0rd',
+    'favoriteClub': 'Salt City Curling Club',
+    'newsLetter': 'false'
+  }
+];
 
 app.get('/', (req, res) => {
-  res.send('Hello, boilerplate!');
+  res
+    .send('A GET Request');
+});
+
+app.get('/user', (req, res) => {
+  res
+    .json(users);
+});
+
+app.post('/', (req, res) => {
+  console.log(req.body);
+  res
+    .send('POST request received');
+});
+
+app.post('/user', (req, res) => {
+  //get the data
+  const { username, password, favoriteClub, newsLetter=false } = req.body;
+
+  //validation code here
+  if(!username) {
+    return res
+      .status(400)
+      .send('Username required');
+  }
+
+  if(!password) {
+    return res
+      .status(400)
+      .send('Password required');
+  }
+
+  if(!favoriteClub) {
+    return res
+      .status(400)
+      .send('Favorite Club required');
+  }
+
+  if (username.length < 6 || username.length > 20) {
+    return res
+      .status(400)
+      .send('Username must be between 6 and 20 characters');
+  }
+
+  //password length
+  if (password.length < 8 || password.length > 36) {
+    return res
+      .status(400)
+      .send('Password must be between 8 and 36 characters');
+  }
+
+  //password contains digit, using regex
+  if (!password.match(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/)) {
+    return res
+      .status(400)
+      .send('Password must contain at least one digit');
+  }
+
+  const clubs = [
+    'Cache Valley Stone Society',
+    'Ogden Curling Club',
+    'Park City Curling Club',
+    'Salt City Curling Club',
+    'Utah Olympic Oval Curling Club'
+  ];
+
+  //make sure the club is valid
+  if (!clubs.includes(favoriteClub)) {
+    return res
+      .status(400)
+      .send('Not a valid club');
+  }
+
+  //after all the validation has passed
+  const id = uuid();// uniquely generated id
+
+  const newUser = {
+    id,
+    username,
+    password,
+    favoriteClub,
+    newsLetter  
+  };
+
+  users.push(newUser);
+
+  res
+    .status(204)
+    .end();
+});
+
+app.delete('/user/:userId', (req, res) => {
+  const { userId } = req.params;
+
+  const index = users.findIndex(u => u.id === userId);
+
+  //make sure we actually found a user with that id
+  if (index === -1) {
+    return res
+      .status(404)
+      .send('User not found');
+  }
+
+  users.splice(index, 1);
+  res.status(204).end();
 });
 
 app.use(function errorHandler(error, req, res, next) {
--- test/app.spec.js
+++ test/app.spec.js
@@ -1,9 +1,98 @@
 const app = require('../src/app');
 
 describe('App', () => {
-  it('Get / responds with 200 containing "Hello, world!"', () => {
-    return supertest(app)
-      .get('/')
-      .expect(200, 'Hello, boilerplate!');
+  describe('GET 200 confirmation testing', () => {
+    it('Get / responds with 200 containing "Hello, world!"', () => {
+      return supertest(app)
+        .get('/')
+        .expect(200, 'A GET Request');
+    });
+    it('GET /user should return an object and respond with 200', () => {
+      return supertest(app)
+        .get('/user')
+        .expect(200)
+        .expect('Content-Type', /json/)
+        .then(res => {
+          expect(res.body).to.be.an('array');
+          expect(res.body[0]).to.be.an('object');
+          expect(res.body[0]).to.include.all.keys('id', 'username', 'password', 'favoriteClub', 'newsLetter');
+        });
+    });
+  });
+  describe('POST', () => {
+    it('POST / responds with 200 containing POST request received', () => {
+      return supertest(app)
+        .post('/')
+        .expect(200, 'POST request received');
+    });
+    describe('Check that the server will reject a bad sign-up', () => {
+      it('POST /user responds with 400 if no username information is entered', () => {
+        const userVals = ['username', 'password', 'newsLetter'];
+        return supertest(app)
+          .post('/user')
+          .expect(400, 'Username required');
+      });
+      it('POST /user responds with 400 if no password information is entered', () => {
+        return supertest(app)
+          .post('/user')
+          .send({username: 'john'})
+          .expect(400, 'Password required');
+      });
+      it('POST /user responds with 400 if no favorite club information is entered', () => {
+        return supertest(app)
+          .post('/user')
+          .send({username: 'john', password: 'steve'})
+          .expect(400, 'Favorite Club required');
+      });
+      it('POST /user responds with 400 if username format is incorrect', () => {
+        return supertest(app)
+          .post('/user')
+          .send({username: 'john', password: 'steve', favoriteClub: 'jobs'})
+          .expect(400, 'Username must be between 6 and 20 characters');
+      });
+      it('POST /user responds with 400 if password format is not long enough', () => {
+        return supertest(app)
+          .post('/user')
+          .send({username: 'johnnywalk', password: 'steve', favoriteClub: 'jobs'})
+          .expect(400, 'Password must be between 8 and 36 characters');
+      });
+      it('POST /user responds with 400 if password doesn\'t include a number', () => {
+        return supertest(app)
+          .post('/user')
+          .send({username: 'johnnywalk', password: 'stevejobs', favoriteClub: 'jobs'})
+          .expect(400, 'Password must contain at least one digit');
+      });
+      it('POST /user responds with 400 if password doesn\'t include a number', () => {
+        return supertest(app)
+          .post('/user')
+          .send({username: 'johnnywalk', password: 'stevejobs1', favoriteClub: 'jobs'})
+          .expect(400, 'Not a valid club');
+      });
+    });
+    it('POST /user responds with 200 for a clean submital', () => {
+      return supertest(app)
+        .post('/user')
+        .send({username: 'johnnywalk', password: 'stevejobs1', favoriteClub: 'Ogden Curling Club'})
+        .expect(204);
+    });
+  });
+  describe('DELETE', () => {
+    it('Should give a 404 when trying to delete a nonexistant user', () => {
+      return supertest(app)
+        .del('/user/3c8da4d5')
+        .expect(404, 'User not found');
+    });
+    it('Should give a 204 when the user is correctly deleted', () => {
+      const users = [{
+        'id': '3c8da4d5-1597-46e7-baa1-e402aed70d80',
+        'username': 'sallyStudent',
+        'password': 'c00d1ng1sc00l',
+        'favoriteClub': 'Cache Valley Stone Society',
+        'newsLetter': 'true'
+      }];
+      return supertest(app)
+        .del('/user/3c8da4d5-1597-46e7-baa1-e402aed70d80')
+        .expect(204);
+    });
   });
 });
